6b44d72b5b15c692f79898046e6d9622d847a6b3
Bonfa <alessio.pinto02@gmail.com>
2025-02-26 10:10:23 +0100

    Added server

---
+++ src/services/server.js
@@ -0,0 +1,102 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Connection error:", err));
+
+import Card from "../models/Card.js";
+
+app.post("/api/add", async (req, res) => {
+  const { id, description, created, time, stopped } = req.body;
+  console.log(req.body);
+  try {
+    const newCard = new Card({
+      id,
+      description,
+      created,
+      time,
+      stopped,
+    });
+
+    await newCard.save();
+    res.status(201).send({ message: "Card added to the DB", card: newCard });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "Error while adding the card in the DB", error: err });
+  }
+});
+
+app.delete("/api/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedCard = await Card.findOneAndDelete({ id: Number(id) });
+    if (!deletedCard) {
+      return res.status(404).send({ message: "Card not found" });
+    }
+
+    res
+      .status(200)
+      .send({ message: "Card deleted successfully", card: deletedCard });
+  } catch (err) {
+    res.status(500).send({ message: "Error deleting card", error: err });
+  }
+});
+
+app.get("/api/getcards", async (req, res) => {
+  try {
+    const foundCards = await Card.find();
+    if (foundCards.length === 0) {
+      return res.status(200).send([]);
+    }
+    res.status(200).send(foundCards);
+  } catch (err) {
+    console.error("Error occurred:", err);
+    res
+      .status(500)
+      .send({ message: "Error occurred while fetching cards", error: err });
+  }
+});
+
+app.put("/api/update", async (req, res) => {
+  const { id, time, stopped } = req.body;
+
+  try {
+    const updatedCard = await Card.findOneAndUpdate(
+      { id },
+      { time, stopped },
+      { new: true }
+    );
+    if (!updatedCard) {
+      return res.status(404).send({ message: "Card not found" });
+    }
+
+    res
+      .status(200)
+      .send({ message: "Card updated successfully", card: updatedCard });
+  } catch (err) {
+    res.status(500).send({ message: "Error updating card", error: err });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta ${port}`);
+});
